5f67f051db0dcc59d4d760e7bc334001b0d2d6f7
Matthew Pourroy <pourroy.matt@gmail.com>
2022-06-01 14:38:03 -0400

    Complete step 1

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+//State of squares on the board
+let board
 
+//Track whose turn it is
+let turn
+
+//See if anyone has won yet, or if a tie occurred
+let winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+//Sqaures on the board 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
